6dc4710fe7fde5c5d6bdf0f89e0deb426b871cbc
Joman_V <German.Vlasenko@bk.ru>
2021-11-09 22:30:28 +0300

    create matrix

--- 11.py
+++ 11.py
@@ -5,9 +5,10 @@ root = Tk()
 c = Canvas(root,width=400,height=300,bg='grey')
 c.pack(side = 'top')
 for i in range(10):
-    x=3
-    y=i*25+3
-    c.create_rectangle(x, y, x+40, y+20)
+    for j in range(10):
+        x=3+45*j
+        y=i*25+3
+        c.create_rectangle(x, y, x+40, y+20)
 
 button = Button(root, text="Quit", command=root.destroy)
 
