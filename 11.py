412d591ddf422f31cf98b69dbf4115d389cbe284
Tricckster <91844832+Tricckster@users.noreply.github.com>
2021-11-13 12:00:32 +0400

    Update 11.py

--- 11.py
+++ 11.py
@@ -2,14 +2,19 @@ from tkinter import *
 
 
 root = Tk()
-c = Canvas(root,width=400,height=300,bg='grey')
+c = Canvas(root,width=400,height=500,bg='grey')
 c.pack(side = 'top')
-for i in range(10):
+
+
+for i in range(5):
     for j in range(10):
         x=3+45*j
         y=i*25+3
-        c.create_rectangle(x, y, x+40, y+20)
+        c.create_rectangle(x, y, x+40, y+20,fill='red')
+
+c.create_rectangle(150, 300, 250, 350,fill='yellow')
 
 button = Button(root, text="Quit", command=root.destroy)
+
 
 root.mainloop()
