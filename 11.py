1acc2283aab702bf8695a5ff8dc0771e82bd9ca0
Joman_V <German.Vlasenko@bk.ru>
2021-11-17 00:09:59 +0300

    switching to Pygame and changing the code

--- 11.py
+++ 11.py
@@ -1,20 +1,43 @@
-from tkinter import *
+import pygame
+ 
+FPS = 60
+W = 400
+H = 700 
+WHITE = (255, 255, 255)
+BLUE = (0, 70, 225)
+BLACK=(0,0,0)
+RIGHT = "to the right"
+LEFT = "to the left"
+STOP = "stop"
+ 
+sc = pygame.display.set_mode((W, H))
+clock = pygame.time.Clock()
 
+x = W // 2 - 40
+y = 600
+while 1:
+    for i in pygame.event.get():
+        if i.type == pygame.QUIT:
+            pygame.quit()
+        elif i.type == pygame.KEYDOWN:
+            if i.key == pygame.K_LEFT:
+                motion = LEFT
+            elif i.key == pygame.K_RIGHT:
+                motion = RIGHT
+        elif i.type == pygame.KEYUP:
+            if i.key in [pygame.K_LEFT,
+                         pygame.K_RIGHT]:
+                motion = STOP
+ 
+    sc.fill(BLACK)
+    pygame.draw.rect(sc, WHITE, (x, y,80,15),2)
+    pygame.display.update()
+ 
+    keys = pygame.key.get_pressed()
 
-root = Tk()
-c = Canvas(root,width=400,height=500,bg='grey')
-c.pack(side = 'top')
-
-
-for i in range(5):
-    for j in range(10):
-        x=3+45*j
-        y=i*25+3
-        c.create_rectangle(x, y, x+40, y+20,fill='red')
-
-c.create_rectangle(150, 300, 250, 350,fill='blue')
-
-button = Button(root, text="Quit", command=root.destroy)
-
-
-root.mainloop()
+    if keys[pygame.K_LEFT]:
+        x -= 5
+    elif keys[pygame.K_RIGHT]:
+        x += 5
+ 
+    clock.tick(FPS)
