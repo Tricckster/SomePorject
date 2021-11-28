817a8d0c118afbc3c905fed1b802787f928e962b
Joman_V <German.Vlasenko@bk.ru>
2021-11-28 15:22:46 +0300

    alfa code complite

--- 11.py
+++ 11.py
@@ -1,32 +1,33 @@
 # library
 import pygame
-import sys
-from random import randrange as rnd
+import random
 
-W,H,FPS=400,700,60
+W,H,FPS=608,1000,60
 # collors
-WHITE = (255, 255, 255)
-BLUE = (0, 70, 225)
+white = (255, 255, 255)
+blue = (0, 70, 225)
+green = (70, 255, 0)
+red = (255, 0, 0)
 BLACK=(0,0,0)
 # plf settings
-plf_w,plf_h = 80,15
+plf_w,plf_h = 100,20
 plf_speed = 15
-plf = pygame.Rect(W // 2 - plf_w // 2,600, plf_w, plf_h)
+plf = pygame.Rect(W // 2 - plf_w // 2,800, plf_w, plf_h)
 # ball settings
-ball_radius = 20
+ball_radius = 10
 ball_speed = 6
 ball_rect = int(ball_radius * 2 ** 0.5)
-ball = pygame.Rect(rnd(ball_rect, W - ball_rect), H // 2, ball_rect, ball_rect)
+ball = pygame.Rect(random.randrange(ball_rect, W - ball_rect), H // 2, ball_rect, ball_rect)
 dx, dy = 1, -1
 # blocks settings
-block_list = [pygame.Rect(7 + 40 * i, 7 + 40 * j, 38, 38) for i in range(10) for j in range(4)]
-color_list = [(rnd(30, 256), rnd(30, 256), rnd(30, 256)) for i in range(10) for j in range(4)]
+block_list = [pygame.Rect(5 + 60 * i, 7 + 30 * j, 55, 26) for i in range(10) for j in range(7)]
+color_list = [(random.randrange(30, 256), random.randrange(30, 256), random.randrange(30, 256)) for i in range(10) for j in range(7)]
 
 pygame.init()
 sc = pygame.display.set_mode((0,0),pygame.FULLSCREEN)
 surf = pygame.Surface((W, H))
 sc.blit(surf, ((sc.get_width()-W)//2,(sc.get_height()-H)//2))
-sc.fill(BLACK)
+
 pygame.mouse.set_visible(False)
 clock = pygame.time.Clock()
 
@@ -51,17 +52,17 @@ def detect_collision(dx, dy, ball, rect):
 while 1:
     for i in pygame.event.get():
         if i.type == pygame.QUIT or (i.type == pygame.KEYDOWN and i.key == pygame.constants.K_ESCAPE):
-            sys.exit()
+            exit()
     
     surf.fill(BLACK)
-
+    sc.fill(BLACK)
     if pygame.mouse.get_focused():
         pos = pygame.mouse.get_pos()
-        pygame.draw.circle(sc, WHITE,(pos),2)
+        pygame.draw.circle(sc, white,(pos),2)
     # drawing world
-    [pygame.draw.rect(surf, color_list[color], block) for color, block in enumerate(block_list)]
-    pygame.draw.rect(surf, pygame.Color('darkorange'), plf)
-    pygame.draw.circle(surf, pygame.Color('white'), ball.center, ball_radius)
+    [pygame.draw.rect(surf, color_list[color], block,2) for color, block in enumerate(block_list)]
+    pygame.draw.rect(surf, white, plf,2)
+    pygame.draw.circle(surf, pygame.Color('white'), ball.center, ball_radius,2)
     # ball movement
     ball.x += ball_speed * dx
     ball.y += ball_speed * dy
@@ -80,10 +81,7 @@ while 1:
         hit_rect = block_list.pop(hit_index)
         hit_color = color_list.pop(hit_index)
         dx, dy = detect_collision(dx, dy, ball, hit_rect)
-        # special effect
-        hit_rect.inflate_ip(ball.width * 3, ball.height * 3)
-        pygame.draw.rect(sc, hit_color, hit_rect)
-        FPS += 2
+        
     # win, game over
     if ball.bottom > H:
         print('GAME OVER!')
@@ -92,15 +90,14 @@ while 1:
         print('WIN!!!')
         exit()
 
-    pygame.draw.rect(surf, WHITE, (0, 0,W-2,H-2),1)
+    pygame.draw.rect(surf, white, (0, 0,W,H),1)
     sc.blit(surf, ((sc.get_width()-W)//2,(sc.get_height()-H)//2))
-    pygame.display.update()
     
-        # control
+    # control
     key = pygame.key.get_pressed()
     if key[pygame.K_LEFT] and plf.left > 0:
         plf.left -= plf_speed
-    if key[pygame.K_RIGHT] and plf.right < W:
+    if key[pygame.K_RIGHT] and plf.right < W-1:
         plf.right += plf_speed
     # update screen
     pygame.display.flip()
